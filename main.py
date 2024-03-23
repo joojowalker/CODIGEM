083541e739c0700fcf3a80d8541a7c3e0a7c1bf5
WorldChanger <joojokojododzi@yahoo.com>
2024-03-24 00:44:51 +0800

    Update main.py
    Modified "filter_triplets" function

--- main.py
+++ main.py
@@ -77,11 +77,13 @@ def get_count(tp, id):
 def filter_triplets(tp, min_uc=5, min_sc=0):
     if min_sc > 0:
         itemcount = get_count(tp, 'movieId')
-        tp = tp[tp['movieId'].isin(itemcount.index[itemcount >= min_sc])]
+        filtered_itemcount = itemcount[itemcount >= min_uc]
+        tp = tp[tp['movieId'].isin(filtered_itemcount.index)]
 
     if min_uc > 0:
         usercount = get_count(tp, 'userId')
-        tp = tp[tp['userId'].isin(usercount.index[usercount >= min_uc])]
+        filtered_usercount = usercount[usercount >= min_uc]
+        tp = tp[tp['userId'].isin(filtered_usercount.index)]
 
     usercount, itemcount = get_count(tp, 'userId'), get_count(tp, 'movieId')
     return tp, usercount, itemcount
